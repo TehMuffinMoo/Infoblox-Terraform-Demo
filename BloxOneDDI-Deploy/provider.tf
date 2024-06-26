11f29f398f634e6c2de2152bef4ff2e524f21159
TehMuffinMoo <mat.cox@tmmn.uk>
2024-06-26 13:59:16 +0100

    provider

--- BloxOneDDI-Deploy/provider.tf
+++ BloxOneDDI-Deploy/provider.tf
@@ -7,12 +7,6 @@ terraform {
   }
 }
 
-# Configure the Azure Provider
-provider "azurerm" {
-  skip_provider_registration = true
-  features {}
-}
-
 # Configure the BloxOne Provider
 provider "bloxone" {
   csp_url = "https://csp.infoblox.com"
