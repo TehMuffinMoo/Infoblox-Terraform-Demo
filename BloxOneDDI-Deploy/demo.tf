586bb5d50f0c3198ac6fd98d486ec0233f734589
TehMuffinMoo <mat.cox@tmmn.uk>
2024-06-26 16:18:14 +0100

    dev

--- BloxOneDDI-Deploy/demo.tf
+++ BloxOneDDI-Deploy/demo.tf
@@ -20,18 +20,18 @@ resource "bloxone_ipam_address_block" "parent_address_block" {
     space = bloxone_ipam_ip_space.ip_space.id
 }
 
-data "bloxone_ipam_next_available_address_blocks" "next_available_address_blocks" {
-  id = bloxone_ipam_address_block.parent_address_block.id
-  address_block_count = var.address_block_count
-  cidr = var.address_block_size
-}
-
-resource "bloxone_ipam_address_block" "address_blocks" {
-    for_each = data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks
-    
-    next_available_id = data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks.id
-    cidr = 24
-    name = var.comment
-    comment = var.comment
-    space = bloxone_ipam_ip_space.ip_space.id
-}
+# data "bloxone_ipam_next_available_address_blocks" "next_available_address_blocks" {
+#   id = bloxone_ipam_address_block.parent_address_block.id
+#   address_block_count = var.address_block_count
+#   cidr = var.address_block_size
+# }
+# 
+# resource "bloxone_ipam_address_block" "address_blocks" {
+#     for_each = data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks
+#     
+#     next_available_id = data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks.id
+#     cidr = 24
+#     name = var.comment
+#     comment = var.comment
+#     space = bloxone_ipam_ip_space.ip_space.id
+# }
