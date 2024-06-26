f307dd4bcacf5c3db386c7f646432e90e1af2365
TehMuffinMoo <mat.cox@tmmn.uk>
2024-06-26 16:29:52 +0100

    test

--- BloxOneDDI-Deploy/demo.tf
+++ BloxOneDDI-Deploy/demo.tf
@@ -12,33 +12,39 @@ resource "bloxone_dns_view" "dns_view" {
   ip_spaces = [bloxone_ipam_ip_space.ip_space.id]
 }
 
-resource "bloxone_ipam_address_block" "parent_address_block" {
-  address = var.parent_address_block
-  cidr = var.parent_address_block_cidr
-  name = "Parent Address Block"
-  comment   = var.comment
-  space = bloxone_ipam_ip_space.ip_space.id
-  dhcp_options = [
-    {
-    option_code  = data.bloxone_dhcp_option_codes.dns_servers.results.0.id
-    option_value = "1.1.1.1"
-    type         = "option"
-    }
-  ]
+# resource "bloxone_ipam_address_block" "parent_address_block" {
+#   address = var.parent_address_block
+#   cidr = var.parent_address_block_cidr
+#   name = "Parent Address Block"
+#   comment   = var.comment
+#   space = bloxone_ipam_ip_space.ip_space.id
+#   dhcp_options = [
+#     {
+#     option_code  = data.bloxone_dhcp_option_codes.dns_servers.results.0.id
+#     option_value = "1.1.1.1"
+#     type         = "option"
+#     }
+#   ]
+# }
+
+data "bloxone_ipam_address_blocks" "parent_address_block" {
+  filters = {
+    name = "Parent Address Block"
+  }
 }
 
-# data "bloxone_ipam_next_available_address_blocks" "next_available_address_blocks" {
-#   id = bloxone_ipam_address_block.parent_address_block.id
-#   address_block_count = var.address_block_count
-#   cidr = var.address_block_size
-# }
-# 
-# resource "bloxone_ipam_address_block" "address_blocks" {
-#     for_each = data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks
-#     
-#     next_available_id = data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks.id
-#     cidr = 24
-#     name = var.comment
-#     comment = var.comment
-#     space = bloxone_ipam_ip_space.ip_space.id
-# }
+data "bloxone_ipam_next_available_address_blocks" "next_available_address_blocks" {
+  id = bloxone_ipam_address_blocks.parent_address_block.id
+  address_block_count = var.address_block_count
+  cidr = var.address_block_size
+}
+
+resource "bloxone_ipam_address_block" "address_blocks" {
+    for_each = data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks
+    
+    next_available_id = data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks.id
+    cidr = 24
+    name = var.comment
+    comment = var.comment
+    space = bloxone_ipam_ip_space.ip_space.id
+}
