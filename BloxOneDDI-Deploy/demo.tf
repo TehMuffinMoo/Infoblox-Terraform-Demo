33d82912c6fae77b374789b724e5af024589e561
TehMuffinMoo <mat.cox@tmmn.uk>
2024-06-26 16:47:30 +0100

    each

--- BloxOneDDI-Deploy/demo.tf
+++ BloxOneDDI-Deploy/demo.tf
@@ -36,7 +36,7 @@ data "bloxone_ipam_next_available_address_blocks" "next_available_address_blocks
 resource "bloxone_ipam_address_block" "address_blocks" {
     for_each = data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks.results
 
-    address = each
+    address = each.value
     cidr = var.subnet_size
     name = var.comment
     comment = var.comment
