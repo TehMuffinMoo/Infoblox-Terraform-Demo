4227e71212f1ce46a18144977074b81f1890406e
TehMuffinMoo <mat.cox@tmmn.uk>
2024-07-01 13:40:33 +0100

    test

--- BloxOneDDI-Deploy/demo.tf
+++ BloxOneDDI-Deploy/demo.tf
@@ -4,6 +4,12 @@ resource "azurerm_subscription" "infobloxlab" {
   billing_scope_id  = data.azurerm_billing_enrollment_account_scope.infobloxlab.id
 }
 
+## Create Azure Resource Group
+resource "azurerm_resource_group" "infobloxlab" {
+  name     = "rg-${var.subscription_description}"
+  location = "UK South"
+}
+
 ## Create Subscription Allocation
 resource "bloxone_ipam_address_block" "address_block" {
     address = data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks.results.0
@@ -38,4 +44,29 @@ resource "bloxone_ipam_subnet" "subnet" {
     tags = {
       Description = "tf-demo"
     }
+}
+
+## Create Virtual Network Security Group
+resource "azurerm_network_security_group" "infobloxlab_nsg" {
+  name                = "${var.subscription_name}-vnet-nsg"
+  location            = azurerm_resource_group.infobloxlab.location
+  resource_group_name = azurerm_resource_group.infobloxlab.name
+}
+
+## Create Virtual Network / Subnet
+resource "azurerm_virtual_network" "example" {
+  name                = "${var.subscription_name}-vnet"
+  location            = azurerm_resource_group.infobloxlab.location
+  resource_group_name = azurerm_resource_group.infobloxlab.name
+  address_space       = [data.bloxone_ipam_next_available_subnets.next_available_address_blocks_child_snet.results.0]
+  dns_servers         = ["1.1.1.1", "1.0.0.1"]
+
+  subnet {
+    name           = "${var.subscription_name}-snet"
+    address_prefix = "${data.bloxone_ipam_next_available_subnets.next_available_address_blocks_child_snet.results.0}/24"
+  }
+
+  tags = {
+    Description = "tf-demo"
+  }
 }
