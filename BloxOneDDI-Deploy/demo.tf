1db728cd977f4c3ebe90700a348e0c018fa8aa1f
TehMuffinMoo <mat.cox@tmmn.uk>
2024-07-01 11:33:51 +0100

    demo

--- BloxOneDDI-Deploy/demo.tf
+++ BloxOneDDI-Deploy/demo.tf
@@ -1,4 +1,4 @@
-resource "bloxone_ipam_address_block" "address_blocks" {
+resource "bloxone_ipam_address_block" "address_block" {
     address = data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks.results.0
     cidr = 22
     name = var.subscription_name
@@ -9,11 +9,22 @@ resource "bloxone_ipam_address_block" "address_blocks" {
     }
 }
 
-resource "bloxone_ipam_address_block" "address_blocks" {
-    address = data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks.results.0
+resource "bloxone_ipam_address_block" "address_block_child" {
+    address = bloxone_ipam_address_block.address_block.id
     cidr = 24
-    name = var.subscription_name
-    comment = var.subscription_description
+    name = "${var.subscription_name}-vnet"
+    comment = "${var.subscription_description} Virtual Network"
+    space = data.bloxone_ipam_ip_spaces.ip_space.results.0.id
+    tags = {
+      Description = "tf-demo"
+    }
+}
+
+resource "bloxone_ipam_subnet" "subnet" {
+    address = bloxone_ipam_address_block.address_block_child.id
+    cidr = 27
+    name = "${var.subscription_name}-snet"
+    comment = "${var.subscription_description} Subnet"
     space = data.bloxone_ipam_ip_spaces.ip_space.results.0.id
     tags = {
       Description = "tf-demo"
