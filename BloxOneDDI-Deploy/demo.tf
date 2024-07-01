d2b10f76ada6f0a6678a98c9d55653c4e01b349b
TehMuffinMoo <mat.cox@tmmn.uk>
2024-07-01 10:42:42 +0100

    tes

--- BloxOneDDI-Deploy/demo.tf
+++ BloxOneDDI-Deploy/demo.tf
@@ -40,7 +40,7 @@ data "bloxone_ipam_next_available_address_blocks" "next_available_address_blocks
 }
 
 resource "bloxone_ipam_address_block" "address_blocks" {
-    address = data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks.results
+    address = data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks.results.0
     cidr = var.subnet_size
     name = var.comment
     comment = var.comment
