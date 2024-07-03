1dac9b3ec9286f1d2396ecab556479ef6bd213db
TehMuffinMoo <mat.cox@tmmn.uk>
2024-07-03 11:43:58 +0100

    test

--- BloxOneDDI-Deploy/demo.tf
+++ BloxOneDDI-Deploy/demo.tf
@@ -20,6 +20,127 @@ resource "bloxone_ipam_address_block" "address_block" {
     tags = {
       Description = "tf-demo"
     }
+    inheritance_sources = {
+      asm_config = {
+        asm_enable_block = {
+          action = "inherit"
+          display_name = "Global DHCP Properties"
+        }
+        asm_growth_block = {
+          action = "inherit"
+          display_name = "Global DHCP Properties"
+        }
+        asm_threshold = {
+          action = "inherit"
+          display_name = "Global DHCP Properties"
+        }
+        forecast_period = {
+          action = "inherit"
+          display_name = "Global DHCP Properties"
+        }
+        history = {
+          action = "inherit"
+          display_name = "Global DHCP Properties"
+        }
+        min_total = {
+          action = "inherit"
+          display_name = "Global DHCP Properties"
+        }
+        min_unused = {
+          action = "inherit"
+          display_name = "Global DHCP Properties"
+      ddns_client_update = {
+        action = "inherit"
+        display_name = "Global DHCP Properties"
+      }
+      ddns_conflict_resolution_mode = {
+        action = "inherit"
+        display_name = "Global DHCP Properties"
+      }
+      ddns_enabled = {
+        action = "inherit"
+        display_name = "Global DHCP Properties"
+      }
+      ddns_hostname_block = {
+        action = "inherit"
+        display_name = "Global DHCP Properties"
+      }
+      ddns_ttl_percent = {
+        action = "inherit"
+        display_name = "Global DHCP Properties"
+      }
+      ddns_update_block = {
+        action = "inherit"
+        display_name = "Global DHCP Properties"
+      }
+      ddns_update_on_renew = {
+        action = "inherit"
+        display_name = "Global DHCP Properties"
+      }
+      ddns_use_conflict_resolution = {
+        action = "inherit"
+        display_name = "Global DHCP Properties"
+      }
+      dhcp_config = {
+        "abandoned_reclaim_time" = {
+          action = "override"
+          display_name = "Global DHCP Properties"
+        }
+        "abandoned_reclaim_time_v6" = {
+          action = "override"
+          display_name = "Global DHCP Properties"
+        }
+        "allow_unknown" = {
+          action = "inherit"
+          display_name = "Global DHCP Properties"
+        }
+        "allow_unknown_v6" = {
+          action = "inherit"
+          display_name = "Global DHCP Properties"
+        }
+        "echo_client_id" = {
+          action = "override"
+          display_name = "Global DHCP Properties"
+        }
+        "filters" = {
+          action = "inherit"
+          display_name = "Global DHCP Properties"
+        }
+        "filters_v6" = {
+          action = "inherit"
+          display_name = "Global DHCP Properties"
+        }
+        "ignore_list" = {display_name = "Global DHCP Properties"
+        }
+        "lease_time" = {
+          action = "inherit"
+          display_name = "Global DHCP Properties"
+        }
+        "lease_time_v6" = {
+          action = "inherit"
+          display_name = "Global DHCP Properties"
+        }
+      }
+      dhcp_options = {
+        action = "inherit"
+      }
+      header_option_filename = {
+        action = "inherit"
+        display_name = "Global DHCP Properties"
+      }
+      header_option_server_address = {
+        action = "inherit"
+        display_name = "Global DHCP Properties"
+      }
+      header_option_server_name = {
+        action = "inherit"
+        display_name = "Global DHCP Properties"
+      }
+      hostname_rewrite_block = {
+        action = "inherit"
+        display_name = "Global DHCP Properties"
+      }
+    }
 }
 
 ## Create Child Address Block for VNET
@@ -59,7 +180,7 @@ resource "azurerm_virtual_network" "example" {
   location            = azurerm_resource_group.infobloxlab.location
   resource_group_name = azurerm_resource_group.infobloxlab.name
   address_space       = [data.bloxone_ipam_next_available_subnets.next_available_address_blocks_child_snet.results.0]
-  dns_servers         = ["1.1.1.1", "1.0.0.1"]
+  dns_servers         = ["1.1.1.1" "1.0.0.1"]
 
   subnet {
     name           = "${var.subscription_name}-snet"
