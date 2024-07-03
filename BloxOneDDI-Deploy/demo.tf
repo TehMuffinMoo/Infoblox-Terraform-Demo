6587e061956a54e354aa1836da080badb0fac2d4
TehMuffinMoo <mat.cox@tmmn.uk>
2024-07-03 14:58:37 +0100

    dev

--- BloxOneDDI-Deploy/demo.tf
+++ BloxOneDDI-Deploy/demo.tf
@@ -111,7 +111,7 @@ resource "azurerm_network_security_group" "infobloxlab_nsg" {
 }
 
 ## Create Virtual Network / Subnet
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "infobloxlab_vnet" {
   name                = "${var.subscription_name}-vnet"
   location            = azurerm_resource_group.infobloxlab.location
   resource_group_name = azurerm_resource_group.infobloxlab.name
