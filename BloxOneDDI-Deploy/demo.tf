33c67b499bff34367dac33f28914b143fbe2d041
TehMuffinMoo <mat.cox@tmmn.uk>
2024-07-03 14:49:58 +0100

    dedv

--- BloxOneDDI-Deploy/demo.tf
+++ BloxOneDDI-Deploy/demo.tf
@@ -115,7 +115,7 @@ resource "azurerm_virtual_network" "example" {
   name                = "${var.subscription_name}-vnet"
   location            = azurerm_resource_group.infobloxlab.location
   resource_group_name = azurerm_resource_group.infobloxlab.name
-  address_space       = ["${trim(data.bloxone_ipam_next_available_subnets.next_available_address_blocks_child_snet.results.0, "\"")}/24"]
+  address_space       = ["${trim(data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks.results.0, "\"")}/24"]
   dns_servers         = ["1.1.1.1", "1.0.0.1"]
 
   subnet {
