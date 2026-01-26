73ebddf95052cf73084dcddd3ec3041598553c3a
TehMuffinMoo <mat.cox@tmmn.uk>
2026-01-26 13:07:14 +0000

    dev

--- BloxOneDDI-Deploy/demo.tf
+++ BloxOneDDI-Deploy/demo.tf
@@ -160,19 +160,16 @@ output "debug_address_string" {
 }
 
 ## Create DNS ACL
-#resource "bloxone_dns_acl" "auth_zone_acl" {
-#  name = "${lower(var.subscription_name)}-acl"
-#  depends_on = [
-#    bloxone_ipam_address_block.address_block_child
-#  ]
-#  list = [
-#    {
-#      access  = "allow"
-#      element = "ip"
-#      address = "${trim(data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks_child.results[0], "\"")}/${data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks_child.cidr}"
-#    },
-#  ]
-#}
+resource "bloxone_dns_acl" "auth_zone_acl" {
+  name = "${lower(var.subscription_name)}-acl"
+  list = [
+    {
+      access  = "allow"
+      element = "ip"
+      address = "${trim(data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks_child.results[0], "\"")}/${data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks_child.cidr}"
+    },
+  ]
+}
 
 ## Create DNS Zone
 resource "bloxone_dns_auth_zone" "auth_zone" {
