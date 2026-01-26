30ef90033bd3ff96bc05ef401c8900355cdb4b7d
TehMuffinMoo <mat.cox@tmmn.uk>
2026-01-26 14:44:56 +0000

    dev

--- BloxOneDDI-Deploy/demo.tf
+++ BloxOneDDI-Deploy/demo.tf
@@ -177,7 +177,7 @@ resource "bloxone_dns_auth_zone" "auth_zone" {
     {
       access  = "allow"
       element = "ip"
-      address = "${trim(data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks_child.results[0], "\"")}/${data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks_child.cidr}"
+      address = "${trim(data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks.results.0, "\"")}/22"
     },
     {
       access  = "deny"
@@ -189,9 +189,6 @@ resource "bloxone_dns_auth_zone" "auth_zone" {
       access  = "deny"
       element = "any"
     },
-  ]
-  depends_on = [
-    azurerm_virtual_network.infobloxlab_vnet
   ]
 }
 
