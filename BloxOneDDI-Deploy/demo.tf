c976634c987ab0f93641b42f34226396c3c62381
TehMuffinMoo <mat.cox@tmmn.uk>
2026-01-26 13:21:02 +0000

    dev

--- BloxOneDDI-Deploy/demo.tf
+++ BloxOneDDI-Deploy/demo.tf
@@ -163,7 +163,7 @@ output "debug_address_string" {
 resource "bloxone_dns_acl" "auth_zone_acl" {
   name = "${lower(var.subscription_name)}-acl"
   depends_on = [
-    data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks_child.results[0]
+    data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks_child
   ]
   list = [
     {
