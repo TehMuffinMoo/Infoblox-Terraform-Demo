d38c26a2e9b235de6b5fe798b37bc8b18ae723a6
TehMuffinMoo <mat.cox@tmmn.uk>
2026-01-26 13:25:53 +0000

    d

--- BloxOneDDI-Deploy/demo.tf
+++ BloxOneDDI-Deploy/demo.tf
@@ -155,25 +155,6 @@ resource "azurerm_virtual_network" "infobloxlab_vnet" {
   }
 }
 
-output "debug_address_string" {
-  value = "${trim(data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks_child.results[0], "\"")}/${data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks_child.cidr}"
-}
-
-## Create DNS ACL
-resource "bloxone_dns_acl" "auth_zone_acl" {
-  name = "${lower(var.subscription_name)}-acl"
-  depends_on = [
-    azurerm_virtual_network.infobloxlab_vnet
-  ]
-  list = [
-    {
-      access  = "allow"
-      element = "ip"
-      address = "${trim(data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks_child.results[0], "\"")}/${data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks_child.cidr}"
-    },
-  ]
-}
-
 ## Create DNS Zone
 resource "bloxone_dns_auth_zone" "auth_zone" {
   fqdn         = "${lower(var.subscription_name)}.${local.region_reverse_map[var.region]}.az.corp.local."
@@ -193,6 +174,11 @@ resource "bloxone_dns_auth_zone" "auth_zone" {
     },
   ]
   update_acl = [
+    {
+      access  = "allow"
+      element = "ip"
+      address = "${trim(data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks_child.results[0], "\"")}/${data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks_child.cidr}"
+    },
     {
       access  = "deny"
       element = "any"
