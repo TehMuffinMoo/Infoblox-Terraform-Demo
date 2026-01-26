2c1870ae87d9df6550b7a8c4a343cc442b13201c
TehMuffinMoo <mat.cox@tmmn.uk>
2026-01-26 12:44:39 +0000

    d

--- BloxOneDDI-Deploy/demo.tf
+++ BloxOneDDI-Deploy/demo.tf
@@ -209,6 +209,6 @@ resource "bloxone_dns_auth_zone" "auth_zone" {
     },
   ]
   depends_on = [
-    bloxone_dns_acl.auth_zone_acl
+    data.bloxone_ipam_next_available_address_blocks.next_available_address_blocks_child
   ]
 }
