ae8cd32844f9d399e1a39a265953b132c755ab74
TehMuffinMoo <mat.cox@tmmn.uk>
2026-01-26 15:37:40 +0000

    lower

--- BloxOneDDI-Deploy/demo.tf
+++ BloxOneDDI-Deploy/demo.tf
@@ -177,7 +177,7 @@ resource "bloxone_dns_acl" "ddns_acl" {
 
 ## Create DNS Zone
 resource "bloxone_dns_auth_zone" "auth_zone" {
-  fqdn         = "${lower(var.subscription_name)}.${local.region_reverse_map[var.region]}.az.corp.local."
+  fqdn         = "${lower(var.subscription_name)}.${lower(local.region_reverse_map[var.region])}.az.corp.local."
   primary_type = "cloud"
   view = "${var.dns_view}"
   # Other optional fields
